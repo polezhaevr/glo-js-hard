66aa324df3c03fc93d9ec9ef26a6409fb7ac0472
Roman <roman@Romans-MacBook-Pro.local>
2025-04-28 21:32:11 +0500

    feat: convert newNum to Number

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ let num = 266219,
     newNum = num.toString().split('').reduce((accumulator, currentValue) =>
         accumulator * currentValue, 1) ** 3;
 
-console.log(newNum.toString().slice(0, 2))
+let sliceNewNum = newNum.toString().slice(0, 2);
+console.log(Number(sliceNewNum));
