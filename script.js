71620c8dc02ff7b35aa344365a6cc25f3a7ad08f
Roman <roman@Romans-MacBook-Pro.local>
2025-04-29 12:36:59 +0500

    feat: great hard lessons

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let num = 266219,
-    newNum = num.toString().split('').reduce((accumulator, currentValue) =>
-        accumulator * currentValue, 1) ** 3;
+'use strict'
+let lang = prompt("Введите 'ru' , или 'en' для вывода дней недели на соотвестующем языке.");
+const languages = {
+        ru: ['Понедельник', 'Вторник', 'Седа', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+        en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    },
+    namePerson = prompt("Введите 'Артем' или 'Александр' , или любое другое имя"),
+    professions = ["Директор", "Преподаватель", "Студент"],
+    result = namePerson == "Артем" ? console.log(professions[0]) :
+    namePerson == "Александр" ? console.log(professions[1]) : console.log(professions[2]);
 
-let sliceNewNum = newNum.toString().slice(0, 2);
-console.log(Number(sliceNewNum));
+if (lang == "ru") {
+    console.log(languages.ru);
+} else if (lang == "en") {
+    console.log(languages.en);
+} else {
+    console.log("Введите либо 'ru' , либо 'en'");
+};
