43ea63bdb154276ad59ed9958be1d8fbc6b625e4
Roman <roman@Romans-MacBook-Pro.local>
2025-04-30 17:37:12 +0500

    feat: solution for hard lesson 4

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 'use strict'
-let lang = prompt("Введите 'ru' , или 'en' для вывода дней недели на соотвестующем языке.");
-const languages = {
-        ru: ['Понедельник', 'Вторник', 'Седа', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-        en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    },
-    namePerson = prompt("Введите 'Артем' или 'Александр' , или любое другое имя"),
-    professions = ["Директор", "Преподаватель", "Студент"],
-    result = namePerson == "Артем" ? console.log(professions[0]) :
-    namePerson == "Александр" ? console.log(professions[1]) : console.log(professions[2]);
+const getLine = function(str) {
+    str != typeof String ? console.log(str.trim().slice(0, 30) + "...") : console.log("Вы ввели не текстовый тип данных!!!");
+}
 
-if (lang == "ru") {
-    console.log(languages.ru);
-} else if (lang == "en") {
-    console.log(languages.en);
-} else {
-    console.log("Введите либо 'ru' , либо 'en'");
-};
+getLine("            это текст-'рыба', часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной 'рыбой' для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum       ")
